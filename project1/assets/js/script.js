c9699e803ff75b208a5a35c366ec5a2a3f984b0b
Aditya <82406617+adigo-tamu@users.noreply.github.com>
2024-09-12 22:51:13 -0500

    changed the button js so now i can toggle with single button instead of two seperate butons

--- project1/assets/js/script.js
+++ project1/assets/js/script.js
@@ -6,13 +6,22 @@ function load_style() {
     document.getElementById("page_style").setAttribute("href", "/project1/assets/css/" + page_style);
 }
 
-function default_style() {
-    localStorage.setItem("page_stylesheet_name", "styles.css");
-    load_style();
-}
+// function default_style() {
+//     localStorage.setItem("page_stylesheet_name", "styles.css");
+//     load_style();
+// }
 
-function new_style() {
-    localStorage.setItem("page_stylesheet_name", "style2.css");
+// function new_style() {
+//     localStorage.setItem("page_stylesheet_name", "style2.css");
+//     load_style();
+// }
+function toggle_style() {
+    let current_style = localStorage.getItem("page_stylesheet_name");
+    if (current_style === "styles.css") {
+        localStorage.setItem("page_stylesheet_name", "style2.css");
+    } else {
+        localStorage.setItem("page_stylesheet_name", "styles.css");
+    }
     load_style();
 }
 
